ba2666b8ef139d496c0054abb2e7c78420a8d726
Matt Johnston <matthew.ryan.johnston@gmail.com>
2018-03-05 08:45:54 -0700

    fixing double review when submitting review

--- db/submit_review.sql
+++ db/submit_review.sql
@@ -1,6 +1,8 @@
 insert into review (trail_id, user_id, review_date, review_text)
 values ($1, $2, $3, $4);
-select review.review_date,
+
+select distinct on (review.review_date)
+        review.review_date,
         review.review_text,
         users.first_name,
         users.last_name,
@@ -8,5 +10,7 @@ select review.review_date,
         rating.rating
 from review
 join users on users.user_id = review.user_id
+join trail on trail.trail_id = review.trail_id and review.user_id = users.user_id
 join rating on rating.trail_id = review.trail_id
-where review.trail_id = $1;
+where review.trail_id = $1
+order by review.review_date desc;
